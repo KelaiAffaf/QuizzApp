0e69a3aa146fab7e895d473632010faf4e2da655
Afaf KELAI <ea_kelai@esi.dz>
2021-04-21 23:39:25 +0100

    final commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import "./assets/style.css"
 import quizzService from "./quizService/index"
-
+import Questionbox from "./components/QuestionBox"
+import Result from "./components/Result"
 class QuizzBee extends Component{
 
     state={
-        questionBank:[]
+        questionBank:[],
+        score:0,
+        responses:0
     };
 
     getQuestions=()=>{
@@ -20,8 +23,31 @@ class QuizzBee extends Component{
        
     }
 
+    computeAnswer=(answer,correctAnswer)=>{
+        if(answer===correctAnswer){
+            this.setState({score:this.state.score+1});
+            
+        }
+
+        this.setState({responses:this.state.responses <5 ? 
+            this.state.responses+1 :5
+        
+        });
+
+    }
+
+
     componentDidMount(){
         this.getQuestions();
+    }
+
+    playAgain=()=>{
+        this.getQuestions();
+        this.setState({
+            score:0,
+            responses:0
+        });
+
     }
     render(){
         return(
@@ -29,13 +55,19 @@ class QuizzBee extends Component{
                 
                 <div className="title">QuizzBee</div>
                 {this.state.questionBank.length>0 &&
+                this.state.responses <5 &&
                 this.state.questionBank.map(
                     ({question,answers,correct,questionId})=>
-                    <h4>
-                        {question}
-                    </h4>
+                   <Questionbox
+                   question={question}
+                   options={answers}
+                   key={questionId}
+                   selected={answer=>this.computeAnswer(answer,correct)}
+                   ></Questionbox>
                 )}
-
+    {this.state.responses=== 5 ?
+    (<Result score={this.state.score} playAgain={this.playAgain}/>)
+    :null}
             </div>
         );
     }
